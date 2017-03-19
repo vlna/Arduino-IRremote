d2576ccb1c6ba76f6f433b64212f5e729d424db4
Vladimír Návrat <vlna@users.noreply.github.com>
2017-03-19 22:16:22 +0100

    Update IRremote.h

--- IRremote.h
+++ IRremote.h
@@ -79,6 +79,9 @@
 #define DECODE_LEGO_PF       0 // NOT WRITTEN
 #define SEND_LEGO_PF         1
 
+#define DECODE_ROOMBA        0 // NOT WRITTEN
+#define SEND_ROOMBA          1
+
 //------------------------------------------------------------------------------
 // When sending a Pronto code we request to send either the "once" code
 //                                                   or the "repeat" code
@@ -119,6 +122,7 @@ typedef
 		DENON,
 		PRONTO,
 		LEGO_PF,
+	        ROOMBA,
 	}
 decode_type_t;
 
@@ -251,6 +255,9 @@ class IRrecv
 #		if DECODE_LEGO_PF
 			bool  decodeLegoPowerFunctions (decode_results *results) ;
 #		endif
+#		if DECODE_ROOMBA
+			bool  decodeRoomba (decode_results *results) ;
+#		endif
 } ;
 
 //------------------------------------------------------------------------------
@@ -338,6 +345,9 @@ class IRsend
 //......................................................................
 #		if SEND_LEGO_PF
 			void  sendLegoPowerFunctions (uint16_t data, bool repeat = true) ;
+#		endif
+#		if SEND_ROOMBA
+			void  sendRoomba (unsigned long data,  int nbits) ;
 #		endif
 } ;
 
